ffbada68157877d30a10e65778df42ae457430ff
malwandla97 <mandlas703@gmail.com>
2024-10-16 14:49:21 +0200

    Add files via upload

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,4 +1,5 @@
 ["innerHTML"]
+["createAddQuoteForm"]
 let quotes = [
     { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Motivation" },
     { text: "Life is what happens when you're busy making other plans.", category: "Life" },
